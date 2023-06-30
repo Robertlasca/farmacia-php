40c795aaabc4be69256d2fc59d36c2ad5a4fd142
Robertlasca <roberto.lasca123@gmail.com>
2023-06-30 03:41:14 -0600

    Model: Conexion and user

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,12 +1,16 @@
 <?php
+    include_once '../model/Usuario.php';
 
     session_start();
 
     $user = $_POST['user'];
     $pass = $_POST['pass'];
 
-    echo $user;
-    echo "";
-    echo $pass;
+    $usuario = new Usuario();
+    $usuario->Loguearse($user,$pass);
+
+    foreach($usuario->objetos as $objeto){
+        print_r($objeto);
+    }
 
 ?>
